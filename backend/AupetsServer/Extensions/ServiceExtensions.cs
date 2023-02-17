a751fcac965ab9beea853e19a64fbb1a90ba71d8
Danilo Santos <santosdanilo2024@gmail.com>
2023-02-17 20:37:52 -0300

    Adicionado pacotes, cors e iniciado loggermanager

---
+++ backend/AupetsServer/Extensions/ServiceExtensions.cs
@@ -0,0 +1,33 @@
+using Contracts;
+using LoggerService;
+
+namespace AupetsServer.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder
+                    .AllowAnyOrigin()  // WithOrigins("dominio")
+                    .AllowAnyMethod()  //WithMethods("GET", "SET")
+                    .AllowAnyHeader()  //WithHeaders("accept", "content-type")
+                );
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options => {
+                
+            });
+        }
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager , LoggerManager>();
+
+        }
+    }
+}
--- backend/AupetsServer/Program.cs
+++ backend/AupetsServer/Program.cs
@@ -1,6 +1,16 @@
+using NLog;
+using AupetsServer.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
 // Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -12,11 +22,24 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+}else
+    app.UseHsts();
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+// Para permitir funcionamento no Linux
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
