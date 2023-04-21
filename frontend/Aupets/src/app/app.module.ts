4a67fb47a45d0c956bbd373f0807b209f9f98606
Danilo Santos <santosdanilo2024@gmail.com>
2023-04-20 22:24:43 -0300

    Criada home no front end

--- frontend/Aupets/src/app/app.module.ts
+++ frontend/Aupets/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
