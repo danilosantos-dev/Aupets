887f5307fe33c7b96c3e5560aaf52cc339fad5a4
Danilo Santos <santosdanilo2024@gmail.com>
2023-04-23 20:43:57 -0300

    Criada pasta components e classe no container da pagina

--- frontend/Aupets/src/app/app.module.ts
+++ frontend/Aupets/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
-import { MenuComponent } from './menu/menu.component';
-import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
