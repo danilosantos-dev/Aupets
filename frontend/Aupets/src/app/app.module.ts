971640931fbe4d2b1bb4244d71edb9b6597b0dc4
Danilo Santos <santosdanilo2024@gmail.com>
2023-04-28 17:35:40 -0300

    Adicionado funcionalidade CallapseModule  ao botão do menu e criado componente de login

--- frontend/Aupets/src/app/app.module.ts
+++ frontend/Aupets/src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CollapseModule } from 'ngx-bootstrap/collapse'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './components/home/home.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
