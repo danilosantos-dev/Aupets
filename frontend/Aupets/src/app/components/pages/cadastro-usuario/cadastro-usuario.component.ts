a721f36deaf820001d86457195df9856bc68a070
Danilo Santos <santosdanilo2024@gmail.com>
2023-05-31 18:03:50 -0300

    Desenvolvimento das validações do login e cadastro

--- frontend/Aupets/src/app/components/pages/cadastro-usuario/cadastro-usuario.component.ts
+++ frontend/Aupets/src/app/components/pages/cadastro-usuario/cadastro-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators  } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-usuario',
@@ -6,5 +7,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./cadastro-usuario.component.css']
 })
 export class CadastroUsuarioComponent {
+
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(){
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+      confirmPassword: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+    })
+  }
+
+  checkName(){
+    return this.registerForm.controls['name'].dirty && this.registerForm.hasError('required', 'name');
+  }
+
+  checkEmail(){
+    return this.registerForm.controls['email'].dirty && this.registerForm.hasError('required', 'email');
+  }
+
+  checkEmailValid(){
+    return this.registerForm.controls['email'].dirty && this.registerForm.hasError('email', 'email');
+  }
+
+  checkPassword(){
+    return this.registerForm.controls['password'].dirty && this.registerForm.hasError('required', 'password');
+  }
+
+  checkPasswordValid(){
+    return this.registerForm.controls['password'].dirty && this.registerForm.hasError('minlength', 'password');
+  }
+
+  checkConfirmPassword(){
+    return this.registerForm.controls['confirmPassword'].dirty && this.registerForm.hasError('required', 'confirmPassword');
+  }
+
+  checkConfirmPasswordValid(){
+    return this.registerForm.controls['confirmPassword'].dirty && this.registerForm.hasError('minlength', 'confirmPassword');
+  }
+
+  onSubmit(){
+    if (this.registerForm.valid) {
+      //ENVIAR DADOS PARA A API
+      console.log(this.registerForm.value)
+    } else {
+      //Disparo do erro
+      this.validateAllFormFields(this.registerForm)
+    }
+  }
+
+   //Percorre o formulario e valida os inputs caso estejam vazios
+   private validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsDirty({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    });
+  }
+
+
+
 
 }
--- frontend/Aupets/src/app/components/pages/login/login.component.ts
+++ frontend/Aupets/src/app/components/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -8,9 +9,54 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
-  constructor(){}
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+    });
+  }
 
   checkEmail(){
-    
+    return this.loginForm.controls['email'].dirty && this.loginForm.hasError('required', 'email');
   }
+
+  checkEmailValid(){
+    return this.loginForm.controls['email'].dirty && this.loginForm.hasError('email', 'email');
+  }
+
+  checkPassword(){
+    return this.loginForm.controls['password'].dirty && this.loginForm.hasError('required', 'password');
+  }
+
+  checkPasswordValid(){
+    return this.loginForm.controls['password'].dirty && this.loginForm.hasError('minlength', 'password');
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      //ENVIAR DADOS PARA A API
+      console.log(this.loginForm.value)
+    } else {
+      //Disparo do erro
+      this.validateAllFormFields(this.loginForm)
+    }
+  }
+
+   //Percorre o formulario e valida os inputs caso estejam vazios
+   private validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsDirty({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    });
+  }
+
+
 }
