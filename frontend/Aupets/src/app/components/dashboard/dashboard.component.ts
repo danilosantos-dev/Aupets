55a29894f13cb1b0827fb96ad108e18d27512665
Danilo Santos <santosdanilo2024@gmail.com>
2023-06-18 23:19:03 -0300

    Criado gráficos para a pagina dashboard

--- frontend/Aupets/src/app/components/dashboard/dashboard.component.ts
+++ frontend/Aupets/src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Chart, registerables } from 'node_modules/chart.js';
+Chart.register(...registerables);
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -8,7 +9,54 @@ import { Component } from '@angular/core';
 export class DashboardComponent {
 
   ngOnInit(){
-
+    this.RenderChart();
   }
 
+  RenderChart(){
+    new Chart("myChart", {
+      type: 'line',
+      data: {
+        labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+        datasets: [{
+          label: 'Numero de visualizações',
+          data: [0, 2, 1, 3, 5, 10],
+          borderWidth: 3,
+          borderColor: 'rgb(255,255,255)',
+
+
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+
+    new Chart("chart-line", {
+      type: 'bar',
+      data: {
+        labels: ['Seg', 'Ter', 'Quar', 'Quin', 'Sex', 'Sab', 'Dom'],
+        datasets: [{
+          label: 'Numero de acessos',
+          data: [1, 1, 3, 5, 4, 6, 3],
+          borderWidth: 3,
+          borderColor: 'rgb(255,255,255)',
+          barPercentage: 0.07,
+          borderRadius: 5
+
+
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
 }
